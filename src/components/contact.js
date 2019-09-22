0e05e651dc4abd71c01981ba99f6546ef00849ef
Peter Garrow <garrow.peter@gmail.com>
2019-09-22 17:56:07 -0600

    Add font awesome icons to contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab, faGithubSquare, faLinkedin, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
 
 const Container = styled.div`
     // margin-top: 3rem;
-    background: linear-gradient(135.93deg, #103973 29.06%, rgba(16, 57, 115, 0.78) 101.99%);
+    background: linear-gradient(327.56deg, #103973 17.22%, rgba(16, 57, 115, 0.78) 113.77%);
     background-blend-mode: darken;
-    height: 200px;
+    // height: 200px;
 `
 
 const Wrapper = styled.div`
@@ -15,12 +19,35 @@ const Wrapper = styled.div`
     padding-top: 5rem;
     color: #fff;
     position: relative;
+    text-align: right;
+`
+
+const IconLink = styled.a`
+    color: #fff;
+`
+
+const Icon = styled(FontAwesomeIcon)`
+    font-size: 3rem;
+    margin-left: 2rem;
 `
 
 const Contact = () => (
     <Container id='contact'>
         <Wrapper>
             <h2 style={{ textAlign: `right` }}>Contact</h2>
+            <IconLink href='https://github.com/garrowp' target='_blank'>
+                <Icon icon={faGithubSquare} />
+            </IconLink>
+            <IconLink href='https://www.linkedin.com/in/peter-garrow/' target='_blank'>
+                <Icon icon={faLinkedin} />
+            </IconLink>
+            <IconLink href='https://twitter.com/PeterGarrow' target='_blank'>
+                <Icon icon={faTwitterSquare} />
+            </IconLink>
+            <IconLink href='mailto:garrow.peter@gmail.com'>
+                <Icon icon={faEnvelope} />
+            </IconLink>
+            
         </Wrapper>
     </Container>
 )
