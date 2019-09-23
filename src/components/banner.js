98e729604f446c55cf876e3af6337f7d576f2d38
Peter Garrow <garrow.peter@gmail.com>
2019-09-22 21:39:45 -0600

    Change banner layout to grid

--- src/components/banner.js
+++ src/components/banner.js
@@ -5,16 +5,19 @@ import ProfileImage from "../components/image"
 import IntroArrow from "../images/intro-arrow.svg"
 import IntroToAbout from "../images/intro-to-about.svg"
 
-const Wrapper = styled.div`
-  height: 625px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+const Container = styled.div`
   position: relative;
+`
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto ${960 / 2}px ${960 / 2}px auto;
+  grid-template-rows: auto auto;
+  grid-column-gap: 2rem;
   margin: 0 auto;
-  max-width: 960px;
-  padding: 0px 1.0875rem 2rem;
-  padding-top: 0;
+  // max-width: 960px;
+  // padding: 0px 1.0875rem 2rem;
+  padding-top: 8rem;
 `
 
 const Intro = styled.div`
@@ -24,57 +27,70 @@ const Intro = styled.div`
 `
 
 const Banner = () => (
-  <Wrapper>
-    <div>
-      <h2>Hi!👋 My name is Peter Garrow.</h2>
-      <Intro>
-        <div>
-          <h3>I'm a </h3>
-        </div>
-        <div>
-          <h3>
-            husband, <br />
-            father, <br />
-            student, &<br />
-            web developer.
-          </h3>
-        </div>
-      </Intro>
-    </div>
-    <div style={{ width: `50%`, position: `relative` }}>
+  <Container>
+    <Wrapper>
       <div
         style={{
-          width: `100%`,
-          background: `rgba(16, 57, 115, 0.89)`,
-          height: `100%`,
-          borderRadius: `4px`,
-          position: `absolute`,
-          top: `-16px`,
-          left: `16px`,
+          justifySelf: `center`,
+          alignSelf: `center`,
+          gridColumn: `2 / 3`,
         }}
-      />
-      <ProfileImage />
-    </div>
-    <img
-      src={IntroArrow}
-      alt="arrow"
-      style={{
-        position: `absolute`,
-        bottom: `16px`,
-        right: `175px`,
-        marginBottom: 0,
-      }}
-    />
-    <img
-      src={IntroToAbout}
-      alt="Intro to about arrow"
-      style={{
-        position: `absolute`,
-        right: `-145px`,
-        bottom: `-80px`,
-      }}
-    />
-  </Wrapper>
+      >
+        <h2>Hi!👋 My name is Peter Garrow.</h2>
+        <Intro>
+          <div>
+            <h3>I'm a </h3>
+          </div>
+          <div>
+            <h3>
+              husband, <br />
+              father, <br />
+              student, &<br />
+              web developer.
+            </h3>
+          </div>
+        </Intro>
+      </div>
+      <div style={{ position: `relative`, gridColumn: `3 / 4` }}>
+        <div
+          style={{
+            width: `100%`,
+            background: `rgba(16, 57, 115, 0.89)`,
+            height: `100%`,
+            borderRadius: `4px`,
+            position: `absolute`,
+            top: `-16px`,
+            left: `16px`,
+          }}
+        />
+        <ProfileImage />
+      </div>
+      <div
+        style={{ gridColumn: `2 / 4`, gridRow: `2 / 2`, textAlign: `center` }}
+      >
+        <img
+          src={IntroArrow}
+          alt="arrow"
+          style={{
+            marginTop: `-3rem`,
+          }}
+        />
+      </div>
+      <div
+        style={{ gridColumn: `4 / 4`, gridRow: `1 / 3`, position: `relative` }}
+      >
+        <img
+          src={IntroToAbout}
+          alt="Intro to about arrow"
+          style={{
+            position: `absolute`,
+            bottom: 0,
+            marginBottom: 0,
+          }}
+        />
+      </div>
+    </Wrapper>
+  </Container>
 )
 
 export default Banner
