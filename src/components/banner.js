18651f4e2013cc6ec4feee19417357d191553299
Peter Garrow <garrow.peter@gmail.com>
2019-09-23 11:35:04 -0600

    Update banner for 1368px media query

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,41 +1,27 @@
 import React from "react"
 import styled from "styled-components"
 
+import {
+  Container,
+  Wrapper,
+  Intro,
+  IntroWrapper,
+  PicWrapper,
+  PicBackground,
+  IntroArrowWrapper,
+  IntroToAboutWrapper,
+  IntroToAboutImg
+} from "./bannerElements"
+
 import ProfileImage from "../components/image"
 import IntroArrow from "../images/intro-arrow.svg"
 import IntroToAbout from "../images/intro-to-about.svg"
-
-const Container = styled.div`
-  position: relative;
-`
-
-const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: auto ${960 / 2}px ${960 / 2}px auto;
-  grid-template-rows: auto auto;
-  grid-column-gap: 2rem;
-  margin: 0 auto;
-  // max-width: 960px;
-  // padding: 0px 1.0875rem 2rem;
-  padding-top: 8rem;
-`
-
-const Intro = styled.div`
-  display: grid;
-  grid-template-columns: max-content max-content;
-  grid-gap: 0.35rem;
-`
+import IntroToAboutSmall from '../images/intro-to-about-small.svg'
 
 const Banner = () => (
   <Container>
     <Wrapper>
-      <div
-        style={{
-          justifySelf: `center`,
-          alignSelf: `center`,
-          gridColumn: `2 / 3`,
-        }}
-      >
+      <IntroWrapper>
         <h2>Hi!👋 My name is Peter Garrow.</h2>
         <Intro>
           <div>
@@ -50,24 +36,12 @@ const Banner = () => (
             </h3>
           </div>
         </Intro>
-      </div>
-      <div style={{ position: `relative`, gridColumn: `3 / 4` }}>
-        <div
-          style={{
-            width: `100%`,
-            background: `rgba(16, 57, 115, 0.89)`,
-            height: `100%`,
-            borderRadius: `4px`,
-            position: `absolute`,
-            top: `-16px`,
-            left: `16px`,
-          }}
-        />
+      </IntroWrapper>
+      <PicWrapper >
+        <PicBackground />
         <ProfileImage />
-      </div>
-      <div
-        style={{ gridColumn: `2 / 4`, gridRow: `2 / 2`, textAlign: `center` }}
-      >
+      </PicWrapper>
+      <IntroArrowWrapper>
         <img
           src={IntroArrow}
           alt="arrow"
@@ -75,20 +49,17 @@ const Banner = () => (
             marginTop: `-3rem`,
           }}
         />
-      </div>
-      <div
-        style={{ gridColumn: `4 / 4`, gridRow: `1 / 3`, position: `relative` }}
-      >
-        <img
-          src={IntroToAbout}
-          alt="Intro to about arrow"
-          style={{
-            position: `absolute`,
-            bottom: 0,
-            marginBottom: 0,
-          }}
-        />
-      </div>
+      </IntroArrowWrapper>
+      <IntroToAboutWrapper>
+        <picture>
+          <source media="(min-width: 1368px)" srcset={IntroToAbout} />
+          <source media="(min-width: 1000px)" srcset={IntroToAboutSmall} />
+          <IntroToAboutImg
+            src={IntroToAbout}
+            alt="Intro to about arrow"
+          />
+        </picture>
+      </IntroToAboutWrapper>
     </Wrapper>
   </Container>
 )
