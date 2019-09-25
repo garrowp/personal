6437bb577ec5db129df271dd9b1f640e3c6f0207
Peter Garrow <garrow.peter@gmail.com>
2019-09-25 11:35:02 -0600

    Change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,17 @@ module.exports = {
     title: `Peter Garrow`,
     description: `My personal site!`,
     author: `Peter Garrow`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/PeterGarrow`
+      },
+      {
+        name: `github`,
+        url: `https://github.com/garrowp`
+      }
+
+    ]
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +35,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/arrow-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
