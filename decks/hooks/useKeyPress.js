9f377a9ea0b55fedcd15a262003fc813080f4660
Peter Garrow <peter.garrow@mx.com>
2021-09-22 08:43:17 -0600

    Check if window is defined

--- decks/hooks/useKeyPress.js
+++ decks/hooks/useKeyPress.js
@@ -1,59 +1,77 @@
-import * as React from "react";
+import * as React from "react"
 
-export function useKeyPress(targetKey) {
-  // State for keeping track of whether key is pressed
-  const [keyPressed, setKeyPressed] =
-    React.useState(false);
-  // If pressed key is our target key then set to true
-  function downHandler({ key }) {
-    if (key === targetKey) {
-      setKeyPressed(true);
-    }
+const isBrowser = typeof window !== "undefined"
+
+// export function useKeyPress(targetKey) {
+//   // if (!isClient) {
+//   //   return false
+//   // }
+//   if (!isBrowser) {
+//     return false
+//   }
+//   // State for keeping track of whether key is pressed
+//   const [keyPressed, setKeyPressed] = React.useState(false)
+//   // If pressed key is our target key then set to true
+//   function downHandler({ key }) {
+//     if (key === targetKey) {
+//       setKeyPressed(true)
+//     }
+//   }
+//   // If released key is our target key then set to false
+//   const upHandler = ({ key }) => {
+//     if (key === targetKey) {
+//       setKeyPressed(false)
+//     }
+//   }
+//   // Add event listeners
+//   React.useEffect(() => {
+//     window.addEventListener("keydown", downHandler)
+//     window.addEventListener("keyup", upHandler)
+//     // Remove event listeners on cleanup
+//     return () => {
+//       window.removeEventListener("keydown", downHandler)
+//       window.removeEventListener("keyup", upHandler)
+//     }
+//   }, []) // Empty array ensures that effect is only run on mount and unmount
+//   return keyPressed
+// }
+
+export function useEventListener(
+  eventName,
+  handler,
+  element = isBrowser ? window : null
+) {
+  // const isClient = typeof window === "object"
+  // if (!isClient) {
+  //   return false
+  // }
+  if (!isBrowser) {
+    return false
   }
-  // If released key is our target key then set to false
-  const upHandler = ({ key }) => {
-    if (key === targetKey) {
-      setKeyPressed(false);
-    }
-  };
-  // Add event listeners
-  React.useEffect(() => {
-    window.addEventListener("keydown", downHandler);
-    window.addEventListener("keyup", upHandler);
-    // Remove event listeners on cleanup
-    return () => {
-      window.removeEventListener("keydown", downHandler);
-      window.removeEventListener("keyup", upHandler);
-    };
-  }, []); // Empty array ensures that effect is only run on mount and unmount
-  return keyPressed;
-}
-
-export function useEventListener(eventName, handler, element = window) {
   // Create a ref that stores handler
-  const savedHandler = React.useRef();
+  const savedHandler = React.useRef()
   // Update ref.current value if handler changes.
   // This allows our effect below to always get latest handler ...
   // ... without us needing to pass it in effect deps array ...
   // ... and potentially cause effect to re-run every render.
   React.useEffect(() => {
-    savedHandler.current = handler;
-  }, [handler]);
+    savedHandler.current = handler
+  }, [handler])
   React.useEffect(
     () => {
       // Make sure element supports addEventListener
       // On
-      const isSupported = element && element.addEventListener;
-      if (!isSupported) return;
+      const isSupported = element && element.addEventListener
+      if (!isSupported) return
       // Create event listener that calls handler function stored in ref
-      const eventListener = (event) => savedHandler.current(event);
+      const eventListener = event => savedHandler.current(event)
       // Add event listener
-      element.addEventListener(eventName, eventListener);
+      element.addEventListener(eventName, eventListener)
       // Remove event listener on cleanup
       return () => {
-        element.removeEventListener(eventName, eventListener);
-      };
+        element.removeEventListener(eventName, eventListener)
+      }
     },
     [eventName, element] // Re-run if eventName or element changes
-  );
+  )
 }
